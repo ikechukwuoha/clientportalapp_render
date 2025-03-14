0a4c8bd4db01bc1db58a57cac98a5af480a2a3cf
Ikechukwu Ohanugodu <ikechukwuandrew4@gmail.com>
2025-03-14 04:49:23 +0100

    Changed base url

--- app/api/config/erp_config.py
+++ app/api/config/erp_config.py
@@ -1,4 +1,5 @@
-FRAPPE_BASE_URL = "http://clientportal.org:8080"
+# FRAPPE_BASE_URL = "http://clientportal.org:8080"
+FRAPPE_BASE_URL="https://lassod.erp.staging.purpledove.net"
 FRAPPE_API_KEY = "your_api_key"
 FRAPPE_API_SECRET = "your_api_secret"
 
