aa4db3abb2c7eb200aed0a884f43f0c2d836b212
Ikechukwu Ohanugodu <ikechukwuandrew4@gmail.com>
2025-03-14 03:54:39 +0100

    Changed Host

--- app/main.py
+++ app/main.py
@@ -95,4 +95,4 @@ async def home():
 
 # Run the app
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_config=None, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_config=None, reload=True)
